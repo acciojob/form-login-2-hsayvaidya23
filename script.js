e1381e715c46cc8daa7fe4480dffecf6d5e8f40d
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-09-01 17:37:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function  submitForm()  {
+const fName = document.getElementById('fName').value;
+const lName = document.getElementById('LName').value;
+const mobile = document.getElementById('mobile').value;
+const email = document.getElementById('emailID').value;
+console.log(fName)
+
+const alertMsg = `First Name: ${fName} Last Name: ${lName} Phone Number: ${mobile} Email ID: ${email}`;
+
+
+	 alert(alertMsg);
+}
